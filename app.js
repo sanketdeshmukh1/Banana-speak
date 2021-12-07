52defad93a7ade7808857ba981fb1e84f7650213
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2021-12-07 19:15:14 +0530

    completed
    completed the code, but will need to improve UI in the next version

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var submitbutton=document.querySelector("#submit-button")
 var txtarea=document.querySelector("#txt-area")
 var txtoutput=document.querySelector("#txt-output")
-function onclicked()
-{
-txtoutput.innerText="output is"+txtarea.value+"afdcsdfvgv";
+var serverurl="https://api.funtranslations.com/translate/minion.json"
+
+function translatedurl(text){
+    return serverurl + "?" + "text="+ text;
 }
+
+function onclicked(){
+    var text1=txtarea.value;
+
+    fetch(translatedurl(text1))
+    .then(response=>response.json())
+    .then(json=>{
+        var finalresult=json.contents.translated;
+        txtoutput.innerText=finalresult;
+    })
+};
 
 submitbutton.addEventListener("click",onclicked)
