8a9793df6056681c44f4b515c65afccf4dbb25f1
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2021-12-07 13:50:43 +0530

    taking input and console it

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-alert("hello");
-prompt("what is your good name");
+var submitbutton=document.querySelector("#submit-button")
+var txtarea=document.querySelector("#txt-area")
+
+function onclicked()
+{
+console.log("button is clicked",)
+console.log("user entered-",txtarea.value);
+}
+
+submitbutton.addEventListener("click",onclicked)
